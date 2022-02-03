cee45f68ecf7572cedd8d94968a79fbff90df8b5
Mie <55872496+mj761@users.noreply.github.com>
2022-02-03 15:54:05 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -3,15 +3,39 @@ console.log('Running assignment...');
 const transactions = require('./simple-transaction-data').data;
 
 // Assignment 1:
-const filterFunction = (data) => {
-    return data.filter((transaction) => {
-        // TODO
-    });
-}
+const result = transactions.filter(transaction => {
+    const year = transaction.transaction_date.substring(0,4);
+    return transaction.credit_card_company == 'VISA'
+    && transaction.price >= 50
+    && year >= 2015
+});
 
+console.log(result);
 
 // Assignment 2:
 const aboveAverageFunction = (data) => {
 
+    let sum = 0;
+    data.forEach(transaction => {
+        sum += parseFloat(transaction.price);
+    });
+
+    let average = sum / data.length;
+    console.log(average);
+
+    return data.map(transaction => {
+        if(transaction.price > average){
+            transaction.isPriceAboveAverage = true;
+            
+            return transaction;
+        }else{
+            transaction.isPriceAboveAverage = false;
+            return transaction;
+        }
+    
+    
+    })
 }
+
+console.log(aboveAverageFunction(transactions));
 
